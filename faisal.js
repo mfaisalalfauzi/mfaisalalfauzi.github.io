0e56c1427d7a84e8cb0fe4fec08f85d1cf81fcbe
M Faisal <muhammadfaisalalfauzi@gmail.com>
2017-11-19 10:07:50 +0700

    update web taman lalu lintas

---
+++ faisal.js
@@ -0,0 +1,20 @@
+// Number
+const n = 10;
+
+// String
+const apos_string = 'hello';
+const quot_string = "hello";
+const tick_string = `hello`;
+
+// Boolean
+
+const b = true
+
+// Array
+
+const r = [1, 2, 3, 4]
+
+// Object
+const rio = {
+    nama:"rio";
+}
